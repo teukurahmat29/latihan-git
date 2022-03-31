2b26447b8395b5e1ffabe35ccfbbcf8e58702223
teukurahmat29 <102155837+teukurahmat29@users.noreply.github.com>
2022-03-31 18:35:25 +0700

    Add files via upload

---
+++ view-latihan1.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>Latihan 1</title>
+</head>
+<body>
+Halo Kawan..Yuk Kita belajar Web Programming..!!!<br>
+Nilai 1 = <?= $nilai1; ?> <br>
+Nilai 2 = <?= $nilai2; ?> <br>
+Ini hasil dari pemodelan dengan methode penjumlahan yaitu <?= $nilai1 . "
+$nilai2 . " = " . $hasil; ?>
+</body>
+</html>
