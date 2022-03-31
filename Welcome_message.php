609400fd76e256628c11374929003bde86242cda
teukurahmat29 <102155837+teukurahmat29@users.noreply.github.com>
2022-03-31 18:32:28 +0700

    Add files via upload

---
+++ Welcome_message.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>welcome to CodeIgniter</title>
+<style type="text/css">
+::selection { background-color:#E13300;color: white:}
+::-moz-selection {background-color: #fff;
+body { 
+background-color: #fff;
+margin: 40px;
+font:13px/20px normal Helvetica, Arial, sans-serif;
+color:#4F5155;
+}
+a {
+	color: #003399;
+	background-color: transparent;
+	font-weight: normal;
+}
