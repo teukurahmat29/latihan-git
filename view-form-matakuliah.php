6cd3677acd7a99107ed32eb2c3698f6e06accf71
teukurahmat29 <102155837+teukurahmat29@users.noreply.github.com>
2022-04-18 08:39:21 +0700

    Update view-form-matakuliah.php

--- view-form-matakuliah.php
+++ view-form-matakuliah.php
@@ -1,56 +1,119 @@
 <html>
 
-<head>
-<title>Form Input Matakuliah</title>
+	<head>
 
-</head>
-<body>
-<center>
-<form action="<?>= base_url('matakuliah/cetak'); ?>" methode="post">
-<table>
+		<title>Form Input Matakuliah</title>
+
+		<style>
+
+			.pesan {color:red;font:italic;}
+
+		</style>
+
+	</head>
+
+	<body>
+
+		<div class="pesan">
+
+<?php echo validation_errors(); ?>
+
+</div>
+
+		<center>
+
+<form action="<?=  base_url('matakuliah/cetak');  ?>" method="post">
+
+	<table>
+
 <tr>
+
 <th colspan="3">
-Form Input Data Mata kuliah
+
+Form Input Data Mata Kuliah
+
 </th>
+
 </tr>
+
 <tr>
-<td colspan="3">
+
+	<td colspan="3">
+
 <hr>
+
 </td>
+
 </tr>
+
 <tr>
+
 <th>Kode MTK</th>
+
 <th>:</th>
+
 <td>
-<input type="text" name="kode" placeholder="input kode" id="kode">
+
+<input type="text" name="kode" id="kode">
+
+	</td>
+
+
+
 </tr>
+
+	<tr>
+
+		<th>Nama MTK</th>
+
+		<th>:</th>
+
+		<td>
+
+			<input type="text" name="nama" id="kode">
+
+		</td>
+
+	</tr>
+
+	<th>SKS</th>
+
+	<th>:</th>
+
+	<td>
+
+		<select name="sks" id="sks">
+
+			<option value="">Pilih SKS</option>
+
+			<option value="2"></option>
+
+			<option value="3"></option>
+
+			<option value="4"></option>
+
+		</select>
+
+	</td>
+
+	</tr>
+
+
+
 <tr>
-<th>Nama MTK</th>
-<td>:</td>
-<td><input type="text" name="nama" id="nama" placeholder="required"></td>
-</tr>
-<tr>
-<th>SKS</th>
-<td>:</td>
-<td>
-<select name="sks">
-<option value="">Pilih SKS</option>
-<option value="2">2</option>
-<option value="3">3</option>
-<option value="4">4</option>
-</select>
-</td>
-</tr>
-<tr>
+
 <td colspan="3" align="center">
 
-<input type="submit" value="Submit">
+<input type="submit" value="submit">
 
 </td>
+
 </tr>
+
 </table>
 
+</form>
+</center>
 </body>
-
-</html>
+</html>	
 		
